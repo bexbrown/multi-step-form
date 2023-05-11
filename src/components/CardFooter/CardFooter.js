60cf76401eb48162ad7d65fe30b9467f5c3c046d
Rebecca Brown <rebecca.brown@msn.com>
2023-05-11 10:21:28 -0400

    style footer for mobile and tablet

--- src/components/CardFooter/CardFooter.js
+++ src/components/CardFooter/CardFooter.js
@@ -12,8 +12,8 @@ function CardFooter({ setStep, step }) {
 
     return (
         <div className="footer">
-            {(step > 1 && step < 5) && <button onClick={handleBackButton} >Go Back</button>}
-            {step < 5 && <button onClick={handleNextButton}>Next Step</button>}
+            {(step > 1 && step < 5) && <button onClick={handleBackButton} className="footer__button footer__button--back" >Go Back</button>}
+            {step < 5 && <button onClick={handleNextButton} className="footer__button footer__button--next">Next Step</button>}
         </div>
     )
 }
