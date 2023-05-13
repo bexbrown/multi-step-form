20000692dce0d3ca66e579ace26a06daf83d30f3
Rebecca Brown <rebecca.brown@msn.com>
2023-05-13 09:55:01 -0400

    fix style on confirm button, set width for all buttons

--- src/components/CardFooter/CardFooter.js
+++ src/components/CardFooter/CardFooter.js
@@ -14,7 +14,7 @@ function CardFooter({ setStep, step }) {
         <div className="footer">
             {(step > 1 && step < 5) && <button onClick={handleBackButton} className="footer__button footer__button--back" >Go Back</button>}
             {step < 4 && <button onClick={handleNextButton} className="footer__button footer__button--next">Next Step</button>}
-            {step === 4 && <button onClick={handleNextButton} className="footer__button footer__button--next">Confirm</button>}
+            {step === 4 && <button onClick={handleNextButton} className="footer__button footer__button--confirm">Confirm</button>}
             {step === 5 && <div className="footer__hidden"></div>}
         </div>
     )
