fa193b31046746e1c4ff703eba6d60e0a92ad29f
Rebecca Brown <rebecca.brown@msn.com>
2023-05-13 09:45:38 -0400

    fix position of step 5 card

--- src/components/CardFooter/CardFooter.js
+++ src/components/CardFooter/CardFooter.js
@@ -15,6 +15,7 @@ function CardFooter({ setStep, step }) {
             {(step > 1 && step < 5) && <button onClick={handleBackButton} className="footer__button footer__button--back" >Go Back</button>}
             {step < 4 && <button onClick={handleNextButton} className="footer__button footer__button--next">Next Step</button>}
             {step === 4 && <button onClick={handleNextButton} className="footer__button footer__button--next">Confirm</button>}
+            {step === 5 && <div className="footer__hidden"></div>}
         </div>
     )
 }
